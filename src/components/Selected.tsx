4ea72aa1a18c96615afe4bc72dcf0467eff5de1a
Alex Burner <alex.burner@gmail.com>
2023-02-26 22:11:14 -0800

    Content/style tweaks

--- src/components/Selected.tsx
+++ src/components/Selected.tsx
@@ -14,46 +14,42 @@ export const Selected: FC = () => {
       <div className="column">
         <div className="pt-3" />
         <div className="message is-info">
-          <div className="message-header is-justify-content-center">
-            Wing {type.relations.wingL.num} — {type.relations.wingL.description}
-          </div>
+          <div className="message-header is-justify-content-center">Wing</div>
+          <MessageBody
+            text={`${type.num}w${type.relations.wingL.num}:  The ${type.relations.wingL.description}`}
+          />
           <TypeCard num={type.relations.wingL.num} />
         </div>
         <div className="message is-danger">
-          <div className="message-header is-justify-content-center">
-            Stress {type.relations.stress.num}
-          </div>
-          <div className="message-body pb-0">
-            Moving in their Direction of Disintegration,{' '}
-            {type.relations.stress.description}.
-          </div>
+          <div className="message-header is-justify-content-center">Stress</div>
+          <MessageBody
+            text={`Moving in their Direction of Disintegration — ${type.relations.stress.description}.`}
+          />
           <TypeCard num={type.relations.stress.num} />
         </div>
       </div>
       <div className="column is-two-fifths">
         <div className="message is-dark is-medium">
-          <div className="message-header is-justify-content-center">
-            Type {type.num}
+          <div className="message-header is-justify-content-center">Type</div>
+          <div className="p-2">
+            <TypeCard num={type.num} />
           </div>
-          <TypeCard num={type.num} />
         </div>
       </div>
       <div className="column">
         <div className="pt-3" />
         <div className="message is-info">
-          <div className="message-header is-justify-content-center">
-            Wing {type.relations.wingR.num} — {type.relations.wingR.description}
-          </div>
+          <div className="message-header is-justify-content-center">Wing</div>
+          <MessageBody
+            text={`${type.num}w${type.relations.wingR.num}:  The ${type.relations.wingR.description}`}
+          />
           <TypeCard num={type.relations.wingR.num} />
         </div>
         <div className="message is-success">
-          <div className="message-header is-justify-content-center">
-            Growth {type.relations.growth.num}
-          </div>
-          <div className="message-body pb-0">
-            Moving in their Direction of Integration,{' '}
-            {type.relations.growth.description}.
-          </div>
+          <div className="message-header is-justify-content-center">Growth</div>
+          <MessageBody
+            text={`Moving in their Direction of Integration — ${type.relations.growth.description}.`}
+          />
           <TypeCard num={type.relations.growth.num} />
         </div>
       </div>
@@ -61,23 +57,65 @@ export const Selected: FC = () => {
   )
 }
 
+const MessageBody: FC<{ text: string }> = ({ text }) => (
+  <div className="message-body pb-0">
+    <div
+      className="pl-2"
+      style={{
+        borderColor: 'inherit',
+        borderLeftStyle: 'solid',
+        borderLeftWidth: '0.2rem',
+        opacity: 0.9,
+      }}
+    >
+      {text}
+    </div>
+  </div>
+)
+
 const TypeCard: FC<{ num: number }> = ({ num }) => {
   const type = types[num - 1]
   if (!type) throw new Error('Unreachable')
   const [currNum, setCurrNum] = useAtom(currNumAtom)
   return (
-    <div className="p-5">
+    <div className="p-5 pb-6 content">
       <h3
-        className={classNames('title is-5', {
+        className={classNames({
           'is-clickable': type.num !== currNum,
         })}
         onClick={() => setCurrNum(type.num)}
       >
-        {type.name}
+        {type.num}: The {type.name}
       </h3>
-      <h5 className="title is-6">{type.summary.subtitle1}</h5>
-      <h5 className="subtitle is-6">{type.summary.subtitle2}</h5>
-      <p>{type.summary.description}</p>
+      <p>
+        <strong>The {type.summary.subtitle1} Type</strong>
+        <br />
+        <em>{type.summary.subtitle2}</em>
+        <br />
+      </p>
+      <div>{type.summary.description}</div>
+      <ul>
+        <li>
+          <strong>Fear: </strong>
+          {type.summary.fear}.
+        </li>
+        <li>
+          <strong>Desire: </strong>
+          {type.summary.desire}.
+        </li>
+        <li>
+          <strong>At their best: </strong>
+          {type.summary.best}
+        </li>
+        <li>
+          <strong>Problems with: </strong>
+          {type.summary.problems}
+        </li>
+        <li>
+          <strong>Key Motivation: </strong>
+          {type.summary.motivation}
+        </li>
+      </ul>
     </div>
   )
 }
--- src/data.ts
+++ src/data.ts
@@ -2,6 +2,8 @@ interface TypeSummary {
   subtitle1: string
   subtitle2: string
   description: string
+  problems: string
+  best: string
   fear: string
   desire: string
   motivation: string
@@ -29,11 +31,13 @@ export const types: Type[] = [
     num: 1,
     name: 'Reformer',
     summary: {
-      subtitle1: 'The Rational, Idealistic Type',
+      subtitle1: 'Rational, Idealistic',
       subtitle2: 'Principled, Purposeful, Self-Controlled, and Perfectionistic',
       description:
-        'Ones are conscientious and ethical, with a strong sense of right and wrong. They are teachers, crusaders, and advocates for change: always striving to improve things, but afraid of making a mistake. Well-organized, orderly, and fastidious, they try to maintain high standards, but can slip into being critical and perfectionistic. They typically have problems with resentment and impatience. At their Best: wise, discerning, realistic, and noble. Can be morally heroic.',
-      fear: 'Of being corrupt/evil, defective',
+        'Ones are conscientious and ethical, with a strong sense of right and wrong. They are teachers, crusaders, and advocates for change: always striving to improve things, but afraid of making a mistake. Well-organized, orderly, and fastidious, they try to maintain high standards, but can slip into being critical and perfectionistic.',
+      problems: 'Resentment and impatience.',
+      best: 'Wise, discerning, realistic, and noble. Can be morally heroic.',
+      fear: 'Being corrupt/evil, defective',
       desire: 'To be good, to have integrity, to be balanced',
       motivation:
         'Want to be right, to strive higher and improve everything, to be consistent with their ideals, to justify themselves, to be beyond criticism so as not to be condemned by anyone.',
@@ -48,7 +52,8 @@ export const types: Type[] = [
         num: 2,
       },
       stress: {
-        description: 'methodical Ones become moody and irrational at Four',
+        description:
+          'methodical Ones become moody and irrational, like unhealthy Fours',
         num: 4,
       },
       growth: {
@@ -62,11 +67,13 @@ export const types: Type[] = [
     num: 2,
     name: 'Helper',
     summary: {
-      subtitle1: 'The Caring, Interpersonal Type',
+      subtitle1: 'Caring, Interpersonal',
       subtitle2: 'Generous, Demonstrative, People-Pleasing, and Possessive',
       description:
-        'Twos are empathetic, sincere, and warm-hearted. They are friendly, generous, and self-sacrificing, but can also be sentimental, flattering, and people-pleasing. They are well-meaning and driven to be close to others, but can slip into doing things for others in order to be needed. They typically have problems with possessiveness and with acknowledging their own needs. At their Best: unselfish and altruistic, they have unconditional love for others.',
-      fear: 'Of being unwanted, unworthy of being loved',
+        'Twos are empathetic, sincere, and warm-hearted. They are friendly, generous, and self-sacrificing, but can also be sentimental, flattering, and people-pleasing. They are well-meaning and driven to be close to others, but can slip into doing things for others in order to be needed.',
+      problems: 'Possessiveness and with acknowledging their own needs.',
+      best: 'Unselfish and altruistic, they have unconditional love for others.',
+      fear: 'Being unwanted, unworthy of being loved',
       desire: 'To feel loved',
       motivation:
         'Want to be loved, to express their feelings for others, to be needed and appreciated, to get others to respond to them, to vindicate their claims about themselves.',
@@ -77,11 +84,12 @@ export const types: Type[] = [
         num: 1,
       },
       wingR: {
-        description: 'Host/Hostess',
+        description: 'Host/ess',
         num: 3,
       },
       stress: {
-        description: 'needy Twos become aggressive and dominating at Eight',
+        description:
+          'needy Twos become aggressive and dominating, like unhealthy Eights',
         num: 8,
       },
       growth: {
@@ -95,11 +103,13 @@ export const types: Type[] = [
     num: 3,
     name: 'Achiever',
     summary: {
-      subtitle1: 'The Success-Oriented, Pragmatic Type',
+      subtitle1: 'Success-Oriented, Pragmatic',
       subtitle2: 'Adaptable, Excelling, Driven, and Image-Conscious',
       description:
-        'Threes are self-assured, attractive, and charming. Ambitious, competent, and energetic, they can also be status-conscious and highly driven for advancement. They are diplomatic and poised, but can also be overly concerned with their image and what others think of them. They typically have problems with workaholism and competitiveness. At their Best: self-accepting, authentic, everything they seem to be—role models who inspire others.',
-      fear: 'Of being worthless',
+        'Threes are self-assured, attractive, and charming. Ambitious, competent, and energetic, they can also be status-conscious and highly driven for advancement. They are diplomatic and poised, but can also be overly concerned with their image and what others think of them.',
+      problems: 'Workaholism and competitiveness.',
+      best: 'Self-accepting, authentic, everything they seem to be—role models who inspire others.',
+      fear: 'Being worthless',
       desire: 'To feel valuable and worthwhile',
       motivation:
         'Want to be affirmed, to distinguish themselves from others, to have attention, to be admired, and to impress others.',
@@ -114,13 +124,14 @@ export const types: Type[] = [
         num: 4,
       },
       stress: {
-        description: 'driven Threes become disengaged and apathetic at Nine',
-        num: 4,
+        description:
+          'driven Threes become disengaged and apathetic, like unhealthy Nines',
+        num: 9,
       },
       growth: {
         description:
           'vain, deceitful Threes become more cooperative and committed to others, like healthy Sixes',
-        num: 7,
+        num: 6,
       },
     },
   },
@@ -128,10 +139,12 @@ export const types: Type[] = [
     num: 4,
     name: 'Individualist',
     summary: {
-      subtitle1: 'The Sensitive, Introspective Type',
+      subtitle1: 'Sensitive, Introspective',
       subtitle2: 'Expressive, Dramatic, Self-Absorbed, and Temperamental',
       description:
-        'Fours are self-aware, sensitive, and reserved. They are emotionally honest, creative, and personal, but can also be moody and self-conscious. Withholding themselves from others due to feeling vulnerable and defective, they can also feel disdainful and exempt from ordinary ways of living. They typically have problems with melancholy, self-indulgence, and self-pity. At their Best: inspired and highly creative, they are able to renew themselves and transform their experiences.',
+        'Fours are self-aware, sensitive, and reserved. They are emotionally honest, creative, and personal, but can also be moody and self-conscious. Withholding themselves from others due to feeling vulnerable and defective, they can also feel disdainful and exempt from ordinary ways of living.',
+      problems: 'Melancholy, self-indulgence, and self-pity.',
+      best: 'Inspired and highly creative, they are able to renew themselves and transform their experiences.',
       fear: 'That they have no identity or personal significance',
       desire:
         'To find themselves and their significance (to create an identity)',
@@ -148,13 +161,14 @@ export const types: Type[] = [
         num: 5,
       },
       stress: {
-        description: 'aloof Fours become over-involved and clinging at Two',
-        num: 4,
+        description:
+          'aloof Fours become over-involved and clinging, like unhealthy Twos',
+        num: 2,
       },
       growth: {
         description:
           'envious, emotionally turbulent Fours become more objective and principled, like healthy Ones',
-        num: 7,
+        num: 1,
       },
     },
   },
@@ -162,10 +176,12 @@ export const types: Type[] = [
     num: 5,
     name: 'Investigator',
     summary: {
-      subtitle1: 'The Intense, Cerebral Type',
+      subtitle1: 'Intense, Cerebral',
       subtitle2: 'Perceptive, Innovative, Secretive, and Isolated',
       description:
-        'Fives are alert, insightful, and curious. They are able to concentrate and focus on developing complex ideas and skills. Independent, innovative, and inventive, they can also become preoccupied with their thoughts and imaginary constructs. They become detached, yet high-strung and intense. They typically have problems with eccentricity, nihilism, and isolation. At their Best: visionary pioneers, often ahead of their time, and able to see the world in an entirely new way.',
+        'Fives are alert, insightful, and curious. They are able to concentrate and focus on developing complex ideas and skills. Independent, innovative, and inventive, they can also become preoccupied with their thoughts and imaginary constructs. They become detached, yet high-strung and intense.',
+      problems: 'Eccentricity, nihilism, and isolation.',
+      best: 'Visionary pioneers, often ahead of their time, and able to see the world in an entirely new way.',
       fear: 'Being useless, helpless, or incapable',
       desire: 'To be capable and competent',
       motivation:
@@ -181,13 +197,14 @@ export const types: Type[] = [
         num: 6,
       },
       stress: {
-        description: 'detached Fives become hyperactive and scattered at Seven',
-        num: 4,
+        description:
+          'detached Fives become hyperactive and scattered, like unhealthy Sevens',
+        num: 7,
       },
       growth: {
         description:
           'avaricious, detached Fives become more self-confident and decisive, like healthy Eights',
-        num: 7,
+        num: 8,
       },
     },
   },
@@ -195,11 +212,13 @@ export const types: Type[] = [
     num: 6,
     name: 'Loyalist',
     summary: {
-      subtitle1: 'The Committed, Security-Oriented Type',
+      subtitle1: 'Committed, Security-Oriented',
       subtitle2: 'Engaging, Responsible, Anxious, and Suspicious',
       description:
-        'Sixes are reliable, hard-working, responsible, and trustworthy. Excellent "troubleshooters," they foresee problems and foster cooperation, but can also become defensive, evasive, and anxious—running on stress while complaining about it. They can be cautious and indecisive, but also reactive, defiant and rebellious. They typically have problems with self-doubt and suspicion. At their Best: internally stable and self-reliant, courageously championing themselves and others.',
-      fear: 'Of being without support and guidance',
+        'Sixes are reliable, hard-working, responsible, and trustworthy. Excellent "troubleshooters," they foresee problems and foster cooperation, but can also become defensive, evasive, and anxious—running on stress while complaining about it. They can be cautious and indecisive, but also reactive, defiant and rebellious.',
+      problems: 'Self-doubt and suspicion.',
+      best: 'Internally stable and self-reliant, courageously championing themselves and others.',
+      fear: 'Being without support and guidance',
       desire: 'To have security and support',
       motivation:
         'Want to have security, to feel supported by others, to have certitude and reassurance, to test the attitudes of others toward them, to fight against anxiety and insecurity.',
@@ -214,7 +233,8 @@ export const types: Type[] = [
         num: 7,
       },
       stress: {
-        description: 'dutiful Sixes become competitive and arrogant at Three',
+        description:
+          'dutiful Sixes become competitive and arrogant, like unhealthy Threes',
         num: 3,
       },
       growth: {
@@ -228,11 +248,13 @@ export const types: Type[] = [
     num: 7,
     name: 'Enthusiast',
     summary: {
-      subtitle1: 'The Busy, Variety-Seeking Type',
+      subtitle1: 'Busy, Variety-Seeking',
       subtitle2: 'Spontaneous, Versatile, Acquisitive, and Scattered',
       description:
-        'Sevens are extroverted, optimistic, versatile, and spontaneous. Playful, high-spirited, and practical, they can also misapply their many talents, becoming over-extended, scattered, and undisciplined. They constantly seek new and exciting experiences, but can become distracted and exhausted by staying on the go. They typically have problems with impatience and impulsiveness. At their Best: they focus their talents on worthwhile goals, becoming appreciative, joyous, and satisfied.',
-      fear: 'Of being deprived and in pain',
+        'Sevens are extroverted, optimistic, versatile, and spontaneous. Playful, high-spirited, and practical, they can also misapply their many talents, becoming over-extended, scattered, and undisciplined. They constantly seek new and exciting experiences, but can become distracted and exhausted by staying on the go.',
+      problems: 'Impatience and impulsiveness.',
+      best: 'They focus their talents on worthwhile goals, becoming appreciative, joyous, and satisfied.',
+      fear: 'Being deprived and in pain',
       desire: 'To be satisfied and content—to have their needs fulfilled',
       motivation:
         'Want to maintain their freedom and happiness, to avoid missing out on worthwhile experiences, to keep themselves excited and occupied, to avoid and discharge pain.',
@@ -248,7 +270,7 @@ export const types: Type[] = [
       },
       stress: {
         description:
-          'scattered Sevens become perfectionistic and critical at One',
+          'scattered Sevens become perfectionistic and critical, like unhealthy Ones',
         num: 1,
       },
       growth: {
@@ -262,11 +284,13 @@ export const types: Type[] = [
     num: 8,
     name: 'Challenger',
     summary: {
-      subtitle1: 'The Powerful, Dominating Type',
+      subtitle1: 'Powerful, Dominating',
       subtitle2: 'Self-Confident, Decisive, Willful, and Confrontational',
       description:
-        "Eights are self-confident, strong, and assertive. Protective, resourceful, straight-talking, and decisive, but can also be ego-centric and domineering. Eights feel they must control their environment, especially people, sometimes becoming confrontational and intimidating. Eights typically have problems with their tempers and with allowing themselves to be vulnerable. At their Best: self- mastering, they use their strength to improve others' lives, becoming heroic, magnanimous, and inspiring.",
-      fear: 'Of being harmed or controlled by others',
+        'Eights are self-confident, strong, and assertive. Protective, resourceful, straight-talking, and decisive, but can also be ego-centric and domineering. Eights feel they must control their environment, especially people, sometimes becoming confrontational and intimidating.',
+      problems: 'Tempers and with allowing themselves to be vulnerable.',
+      best: "Self- mastering, they use their strength to improve others' lives, becoming heroic, magnanimous, and inspiring.",
+      fear: 'Being harmed or controlled by others',
       desire:
         'To protect themselves (to be in control of their own life and destiny)',
       motivation:
@@ -283,7 +307,7 @@ export const types: Type[] = [
       },
       stress: {
         description:
-          'self-confident Eights become secretive and fearful at Five',
+          'self-confident Eights become secretive and fearful, like unhealthy Fives',
         num: 5,
       },
       growth: {
@@ -297,11 +321,13 @@ export const types: Type[] = [
     num: 9,
     name: 'Peacemaker',
     summary: {
-      subtitle1: 'The Easygoing, Self-Effacing Type',
+      subtitle1: 'Easygoing, Self-Effacing',
       subtitle2: 'Receptive, Reassuring Agreeable, and Complacent',
       description:
-        'Nines are accepting, trusting, and stable. They are usually creative, optimistic, and supportive, but can also be too willing to go along with others to keep the peace. They want everything to go smoothly and be without conflict, but they can also tend to be complacent, simplifying problems and minimizing anything upsetting. They typically have problems with inertia and stubbornness. At their Best: indomitable and all-embracing, they are able to bring people together and heal conflicts.',
-      fear: 'Of loss and separation',
+        'Nines are accepting, trusting, and stable. They are usually creative, optimistic, and supportive, but can also be too willing to go along with others to keep the peace. They want everything to go smoothly and be without conflict, but they can also tend to be complacent, simplifying problems and minimizing anything upsetting.',
+      problems: 'Inertia and stubbornness.',
+      best: 'Indomitable and all-embracing, they are able to bring people together and heal conflicts.',
+      fear: 'Loss and separation',
       desire: 'To have inner stability "peace of mind"',
       motivation:
         'Want to create harmony in their environment, to avoid conflicts and tension, to preserve things as they are, to resist whatever would upset or disturb them.',
@@ -316,7 +342,8 @@ export const types: Type[] = [
         num: 1,
       },
       stress: {
-        description: 'complacent Nines become anxious and worried at Six',
+        description:
+          'complacent Nines become anxious and worried, like unhealthy Sixes',
         num: 6,
       },
       growth: {
