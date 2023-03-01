d625257049d1541a598d88e915f0a66935199468
Alex Burner <alex.burner@gmail.com>
2023-02-28 18:41:21 -0800

    Fix mobile overflow bug

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,10 @@ import { Content } from './components/Content'
 import { Diagram } from './components/Diagram'
 
 export const App: FC = () => (
-  <div className="mx-auto pt-1 px-5 pb-6" style={{ maxWidth: '2000px' }}>
+  <div
+    className="mx-auto pt-1 px-5 pb-6"
+    style={{ maxWidth: '2000px', overflow: 'hidden' }}
+  >
     <div className="pb-6">
       <Diagram />
       <Content />
